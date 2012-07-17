cc36ee665be8949d13dec51ddf409e6444927aab
Martin Varela <martin.varela@vtt.fi>
2012-07-17 13:16:11 +0300

    added extra copy to clipboard on pagestats to fix strange behaviour. This needs to be further checked

--- collect_timings.rb
+++ collect_timings.rb
@@ -5,13 +5,13 @@
 ## The information is saved in HAR (JSON-based) format in the 'out' directory, under the website's name, and with file names corresponding to timestamps.
 
 # Sites to visit
-sites = ["www.google.com", "www.hs.fi", "www.yle.fi", "blog.flickr.net/en", "en.wikipedia.org/wiki/Main_Page", "www.amazon.com", "www.ebay.com", "www.youtube.com", "www.imdb.com", "www.bbc.co.uk/news"]
+sites = ["www.google.com", "www.yle.fi", "blog.flickr.net/en", "en.wikipedia.org/wiki/Main_Page", "www.amazon.com", "www.ebay.com", "www.youtube.com", "www.imdb.com", "www.bbc.co.uk/news", "www.npr.org"]
 
 # time between visits (in s)
 interval = 600
 
 # overall period (every ten mintes, for 3 days)
-timeout = interval * 6 * 24 * 3
+timeout = interval * 6 * 24 #* 3
 
 # Do our stuff
 
@@ -19,7 +19,7 @@ while timeout > 0
 
   sites.each do |s|
     out_name = Dir.pwd + "/out/" + s.gsub(/[^A-z]/,'_') + "-" +Time.now.to_s + ".har"
-    system "osascript Pagestats.scpt \"http://#{s}\" \"#{out_name}\""
+    system "osascript Pagestats.scpt \'http://#{s}\' \'#{out_name}\'"
   end
   
   sleep interval
